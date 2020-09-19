bec00efc384ccb84dfb3a6d548710b7259722371
YannChye <yannying.chye@monash.edu>
2020-09-19 13:03:33 +1000

    update map.js

--- 03_dashboard/static/js/map.js
+++ 03_dashboard/static/js/map.js
@@ -1,9 +1,12 @@
-const url="/api"
+// initial parameter
+var url="/api/population";
+var year=2019;
+
 const coord="https://raw.githubusercontent.com/datasets/geo-countries/master/data/countries.geojson"
 
 var myMap = L.map("map", {
   center: [39.5501, -105.7821],
-  zoom: 5
+  zoom: 3
 });
 
 L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
@@ -13,58 +16,81 @@ L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?acce
   accessToken: API_KEY
 }).addTo(myMap);
 
-var year=2019;
-var age=0;
 
-var populationdata=[];
-function getPop() {
+// get data by year
+var mapdata=[];
+function getData() {
   d3.json(url).then(function(data) {
+    var maxVal=getMaxVal(data)
     data.forEach(function(data) {
-      if (data.year==year && data.age==age) {
-        populationdata.push(data)
+      if (data.year==year) {
+        mapdata.push(data)
       }
     })
-    buildMap(populationdata)
+    buildMap(mapdata,maxVal)
     })
 }
-function buildMap(populationdata) {
+
+//get maximum value
+function getMaxVal(data) {  
+  let valPrecision=Math.round(data[data.length-1]["Population"]).toString().length - 1;
+  maxVal=data[data.length-1]["Population"]
+  for (i=0;i<valPrecision-1;i++) {
+    maxVal=maxVal/10
+  }
+  maxVal=Math.ceil(maxVal)
+  for (i=0;i<valPrecision-1;i++) {
+    maxVal=maxVal*10
+  }
+  return maxVal
+}
+
+// get map colour
+function chooseColor(maxVal,feature) {
+  if (feature > maxVal/10) { //note to self: have to change condition for different variables (population v death v life etc)
+    return "#218173"
+  }
+  else if (feature > maxVal/100) {
+    return "#7fc7ae"
+  }
+  else if (feature > maxVal/1000) {
+    return "#d2dfdc"
+  }
+  else if (feature > maxVal/10000) {
+    return "#99bbd6"
+  }
+  else if (feature > 0) {
+    return "#497c94"
+  }
+  else {
+    return "white"
+  }
+}
+
+// build map from data
+function buildMap(mapdata,maxVal) {
   d3.json(coord).then(function(data) {
     var coordinates=data.features;
     for (i=0;i<coordinates.length;i++) {
-      for (j=0;j<populationdata.length;j++) {
-        if (populationdata[j].iso3_code==coordinates[i].properties.ISO_A3) {
-          coordinates[i].properties.population=populationdata[j].population_total_thousands;
+      for (j=0;j<mapdata.length;j++) {
+        if (mapdata[j].iso3_code==coordinates[i].properties.ISO_A3) {
+          coordinates[i].properties.varOfInterest=mapdata[j].Population;
+          coordinates[i].properties.region=mapdata[j].sdg;
         }}
-        var feature=coordinates[i];
-      //var iso=feature.properties.ISO_A3;
-      //for (i=0;i<populationdata.length;i++) {
-        //if (populationdata[i].iso3_code==iso) {
-          //var number=Math.round(populationdata[i].population_total_thousands)}}
-        L.geoJSON(feature).bindPopup("<p>"+feature.properties.population+"</p>").addTo(myMap)}
-    //   //for (j=0;j<populationdata.length;j++) {
-    //     //if (coordinates[i].properties.ISO_A3==populationdata[j].iso3_code) {
-    //       var array=[]
-    //       for (var k=0;k<coordinates[i].geometry.coordinates[0][0].length;k++) {
-    //         array.push([coordinates[i].geometry.coordinates[0][0][k][1],coordinates[i].geometry.coordinates[0][0][k][0]])
-    //       }
-    //       L.polygon(array).bindPopup("<p>"+coordinates[i].properties.ISO_A3+"</p>").addTo(myMap)
-    //     }
-      //}
-    //}
-    // coordinates.forEach(function(data) {
-    //   for (i=0;i<populationdata.length;i++) {
-    //     if (populationdata[i].iso3_code==data.properties.ISO_A3) {
-    //       var array=[]
-    //       for (var i=0;i<data.geometry.coordinates.length;i++) {
-    //         array.push([data.geometry.coordinates[i][1],data.geometry.coordinates[i][0]])
-    //         console.log(array[0][1])
-    //         L.polygon(array[0][1]).addTo(myMap)
-    //       }
-    //       break
-    //     }
-    //   }
-    // })
-    //geojson=L.choloropleth(data)
+      var feature=coordinates[i];
+        L.geoJSON(feature, {
+          style: function(feature) {
+            return {
+              fillColor:chooseColor(maxVal,feature.properties.varOfInterest),
+              fillOpacity:0.8,
+              weight:0
+            }
+          }
+        }).bindPopup(
+          "<p><b>"+feature.properties.ADMIN+"</b></p>"+"<hr>"+
+          "<p><b>region</b>: "+feature.properties.region+"</p>"+
+          "<p><b>population</b>: "+(feature.properties.varOfInterest===undefined?"no data":(feature.properties.varOfInterest*1000).toLocaleString())+"</p>")
+          .addTo(myMap)}
   });
 }
-getPop()
+getData()
