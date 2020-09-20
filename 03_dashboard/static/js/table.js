afb10e4e1211108e6ba85915e855954b082e5102
HockingM <64179310+HockingM@users.noreply.github.com>
2020-09-20 18:31:11 +1000

    Update table for bar colour changes

--- 03_dashboard/static/js/table.js
+++ 03_dashboard/static/js/table.js
@@ -3,10 +3,10 @@ var getYear = 2017;
 //var age = 0;
 
 var populationdata = []
-var getCountry = "Mexico"
+var getCountry = "India"
 var barCountry = []
 var barData = []
-
+var barColours = ["#006666", "#006666", "#339999", "#006666","#006666"]
 
 function getBar() {
     d3.json(url).then(function (data) {
@@ -20,13 +20,21 @@ function getBar() {
             if (populationdata[i].country == getCountry) {
                 let tableRow = i;
                 if (i == 0) {
-                    tableRow = (i + 2)
+                   tableRow = (i + 2);
+                   barColours[2] = "#006666";
+                   barColours[4] = "#339999";
                 } else if (i == 1) {
-                    tableRow = (i + 1)
+                    tableRow = (i + 1);
+                    barColours[2] = "#006666";
+                    barColours[3] = "#339999";
                 } else if (i == populationdata.length - 2) {
-                    tableRow = (i - 1)
+                    tableRow = (i - 1);
+                    barColours[2] = "#006666";
+                    barColours[1] = "#339999";
                 } else if (i == populationdata.length - 1) {
-                    tableRow = (i - 2)
+                    tableRow = (i - 2);
+                    barColours[2] = "#006666";
+                    barColours[0] = "#339999";
                 } else {
                     tableRow = i
                 }
@@ -40,10 +48,12 @@ function getBar() {
                 barData.push(populationdata[tableRow].Population * 1000);
                 barData.push(populationdata[tableRow - 1].Population * 1000);
                 barData.push(populationdata[tableRow - 2].Population * 1000);
+
             };
         };
-        console.log(barData);
+        console.log(barCountry);
         // create array for bar chart
+
         var trace1 = [{
             type: "bar",
             x: barData,
@@ -51,7 +61,7 @@ function getBar() {
             orientation: "h",
             hoverinfo: barData,
             marker: {
-                color: ["#006666", "#006666", "#339999", "#006666", "#006666"]
+                color: barColours
             }
         }];
         Plotly.newPlot("bar", trace1);
