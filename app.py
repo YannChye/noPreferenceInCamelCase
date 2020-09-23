65669cf5dac6185a178343419cfc6c7920ed7410
YannChye <40221679+YannChye@users.noreply.github.com>
2020-09-23 19:58:49 +1000

    Update app.py

--- app.py
+++ app.py
@@ -19,7 +19,10 @@ app = Flask(__name__)
 # Database Setup
 #################################################
 
-DATABASE_URL='postgresql://'+username+':'+password+'@localhost/world_population'
+# DATABASE_URL will contain the database connection string:
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', '')
+# Connects to the database using the app config
+db = SQLAlchemy(app)
 
 # create and connect to engine
 engine=create_engine(DATABASE_URL)
